a45af35516b2d687a9c7d5043b33ffe98935d546
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2024-11-29 10:09:30 +0100

    asd

--- Program.cs
+++ Program.cs
@@ -2,3 +2,14 @@
 
 string name = "Zoltan";
 Console.WriteLine($"Hello {name}!");
+
+ConsoleColor[] colors = 
+    [ConsoleColor.Red, ConsoleColor.Green,
+    ConsoleColor.Blue, ConsoleColor.Yellow];
+
+for (int i = 0; i < 10; i++)
+{
+    Console.ForegroundColor = colors[colors.Length % 4];
+    Console.Write("random text ");
+}
+
